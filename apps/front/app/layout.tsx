0d0bdafd45f8b4a1bffa91e02fe3389b30024659
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2025-02-12 17:07:36 -0300

    chore: starter project

---
+++ apps/front/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Smart Orquestra",
+  description: "Desenvolvido por Mariosa Tech",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
