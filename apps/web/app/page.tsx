d4945a9d6964d4d489f9b77520645b3d50ffe0f3
Silas Roberto Mariosa de Vasconcelos <113197484+silas-mariosa@users.noreply.github.com>
2025-02-12 13:41:33 -0300

    deleted

--- apps/web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1>Smart Orquestra</h1>
-    </div>
-  );
-}
